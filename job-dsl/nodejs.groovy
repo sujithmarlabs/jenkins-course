dfa22f8ded1e835c0be38fa62eea5f5c76f2d33d
sujithmarlabs <sujith.marlabs@gmail.com>
2022-07-27 21:59:54 +0530

    Update nodejs.groovy
    repo and email updated

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/sujithmarlabs/docker-demo-nodejs.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('jenkins-dsl@sujith_jenkins.com')
         }
     }
     triggers {
